947dc3d77aecbf2ce2cf1a2c0351cdd0558afd4b
Shubhranshu Panda <shuboypanda@gmail.com>
2023-03-06 09:50:48 +0530

    2nd committed on 06032023

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log('hello function');
     return 'CRUD Rest API';
   }
 }
